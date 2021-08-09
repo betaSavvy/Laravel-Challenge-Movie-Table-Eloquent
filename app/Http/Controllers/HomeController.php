8afbefad6b077827892ac5eb5e1ae499989acca2
Delonn Koh <delonnkoh.dev@gmail.com>
2021-08-09 16:41:20 +0800

    Improve eloquent query and performance

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Movie;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $movies = Movie::all()->sortByDesc(function($movie) {
-            return $movie->ratings->avg('rating');
-        })->take(100);
+//        $movies = Movie::all()->sortByDesc(function($movie) {
+//            return $movie->ratings->avg('rating');
+//        })->take(100);
+
+        $movies = Movie::select(['id', 'category_id', 'title', 'release_year'])
+                       ->with('category:id,name')
+                       ->withAvg('ratings', 'rating')
+                       ->withCount('ratings')
+                       ->limit(100)
+                       ->get();
 
         return view('home', compact('movies'));
     }
